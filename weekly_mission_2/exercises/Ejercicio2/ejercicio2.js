7b34eba472acf361087a0807fbfcc1b80a6d77fa
AndreaCuriel <acurielipn@hotmail.com>
2022-04-19 14:13:41 -0500

    use every

--- weekly_mission_2/exercises/Ejercicio2/ejercicio2.js
+++ weekly_mission_2/exercises/Ejercicio2/ejercicio2.js
@@ -51,28 +51,28 @@ const explorers = [
     },
   },
 ];
-//show name for each explorer (forEach)
+//1.- show name for each explorer (forEach)
 explorers.forEach((names) => console.log(names.name));
 
-//show stack for each explorer (forEach)
+//2.- show stack for each explorer (forEach)
 explorers.forEach((stacks) => console.log(stacks.stack));
 
-//create a new stack list for each explorer (map)
+//3.- create a new stack list for each explorer (map)
 const listStack = explorers.map((list) => list.stack);
 console.log("NEW STACK LIST");
 console.log(listStack);
 
-//create a mew list where it has only js like stack (filter)
+//4.- create a mew list where it has only js like stack (filter)
 const listJsStack = explorers.filter((list) => list.stack.includes("js"));
 console.log("FILTER ONLY JS STACK");
 console.log(listJsStack);
 
-//first explorer that they are from CDMX with FIND
+//5.- first explorer that they are from CDMX with FIND
 const firstExplorer = explorers.find((first) => first.city === "CDMX");
 console.log("FIRST EXPLORER IS FROM CDMX");
 console.log(firstExplorer);
 
-//obtain the sum of the all exercises completed with REDUCE
+//6.- obtain the sum of the all exercises completed with REDUCE
 const sumExercises = explorers.reduce(
   (acc, exe) => acc + exe.exercises_completed,
   0
@@ -80,9 +80,16 @@ const sumExercises = explorers.reduce(
 console.log("SUM ALL EXERCISES COMPLETED ");
 console.log(sumExercises);
 
-//VALIDATE IF SOME EXPLORER HAS exercisesFinished IN frontend LIKE true (USE some)
+//7.- VALIDATE IF SOME EXPLORER HAS exercisesFinished IN frontend LIKE true (USE some)
 const someExplorer = explorers.some(
   (finish) => finish.missions.frontend.exercisesFinished === true
 );
 console.log("SOME EXPLORER HAS EXERCISES FINISHED IN FRONTEDN MISSION");
 console.log(someExplorer);
+
+//8.- If all explorers have in isFinished of onboarding like true. (every)
+const onbordingFinished = explorers.every(
+  (onbo) => onbo.missions.onboarding.isFinished === true
+);
+console.log("ALL EXPLORERS HAVE DONE THE ONBOARDING");
+console.log(onbordingFinished);
